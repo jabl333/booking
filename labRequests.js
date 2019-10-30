ac45eb183e1e6d9cba74957da0f6fa9cc14fbac9
jabl333 <51697605+jabl333@users.noreply.github.com>
2019-10-30 13:42:44 +0100

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,5 @@
 var labEvents = [ 
+  { title: 'Clearing Regulatory Framework / Jana B.', start: '2019-11-01T09:00:00', end: '2019-11-01T18:00:00',resourceId: 'C-MEETROOM'},
   { title: 'CR+StX Dept. Workshop / Katka V.', start: '2019-11-26T10:00:00', end: '2019-11-26T12:00:00',resourceId: 'B-LONGTABLE'},
   { title: 'XBID Backlog Refinement / Jiri V.', start: '2019-10-31T10:00:00', end: '2019-10-31T11:00:00',resourceId: 'C-MEETROOM'},
   { title: 'Waste Sorting Training / Bara S.', start: '2020-01-22T10:30:00', end: '2019-01-22T12:00:00',resourceId: 'B-LONGTABLE'},
